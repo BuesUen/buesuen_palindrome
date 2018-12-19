5398fbe55b5ddc6cbc320ed6b5b279bbd50440a3
BuesUen <buesuen@gmail.com>
2018-12-19 13:39:24 +0100

    finish workung and refactroedpalindrome method

--- lib/buesuen_palindrome.rb
+++ lib/buesuen_palindrome.rb
@@ -1,6 +1,18 @@
 require "buesuen_palindrome/version"
 
-module BuesuenPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
